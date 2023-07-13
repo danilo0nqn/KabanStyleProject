bd28e020f32c3d0342898cff3e0ac89ae9bb78b7
Danilo Del Collado <daniloddc.92@gmail.com>
2023-07-13 19:53:26 -0300

    Almost done with homePage

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  IRandomContact,
-  SingleResult,
-} from 'src/app/models/randomuser.interface';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { IRandomContact , SingleResult } from 'src/app/models/randomuser.interface';
 import { RandomProfileService } from 'src/app/services/random-profile.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -12,7 +9,14 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+
   userData!: IRandomContact;
+  userPersonalStatus: string = ''
+  canChangePersonalStatus:boolean = false
+
+  //Agregue esto para poder guardar el h2 editable en el estado del usuario
+  @ViewChild('userPersonalStatusElement') userPersonalStatusElement: ElementRef | undefined;
+
   constructor(
     private randomProfile: RandomProfileService,
     private route: ActivatedRoute
@@ -24,9 +28,18 @@ export class HomePageComponent implements OnInit {
     this.route.data.subscribe((data) => {
       const randomContact: SingleResult = data['randomContact'];
     });
+    this.userPersonalStatus = `Hi! This is ${this.userData.first_name}! Currently working so you can message me throug LinkedIn!`
   }
 
-  getAvatar() {
-    console.log(this.userData.avatar);
+  changePersonalStatus(){
+    this.canChangePersonalStatus = !this.canChangePersonalStatus
   }
+
+  saveChangePersonalStatus(){
+    if (this.userPersonalStatusElement?.nativeElement) {
+      this.userPersonalStatus = this.userPersonalStatusElement.nativeElement.innerText;
+    }
+    this.canChangePersonalStatus = !this.canChangePersonalStatus
+  }
+
 }
