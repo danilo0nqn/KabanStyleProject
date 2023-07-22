6dd4aa824f1c59e48ef787d772e6d95ce467c34a
Danilo Del Collado <daniloddc.92@gmail.com>
2023-07-22 02:08:45 -0300

    Added new project form and popup component when selecting owner of project to see the profile details.

--- src/app/components/taskPage/pending-tasks/pending-tasks.component.ts
+++ src/app/components/taskPage/pending-tasks/pending-tasks.component.ts
@@ -1,16 +1,33 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, ViewChild, ViewContainerRef, ComponentRef, QueryList, ViewChildren, OnInit } from '@angular/core';
 import { Assignment } from 'src/app/models/assignment';
+import { ContactPopupService } from 'src/app/services/contact-popup.service';
 
 @Component({
   selector: 'app-pending-tasks',
   templateUrl: './pending-tasks.component.html',
   styleUrls: ['./pending-tasks.component.scss']
 })
-export class PendingTasksComponent implements OnChanges{
+export class PendingTasksComponent implements OnInit{
+  @ViewChildren('popupContainer', { read: ViewContainerRef}) public popupContainers!: QueryList<ViewContainerRef>;
   /* TODO: Cuadno cargo una tarea, guardar el ID del owner para la ventana popup */
   @Input() pendingAssignments!: Assignment[]
+  ownerOpen:boolean = true;
 
-  ngOnChanges(changes: SimpleChanges): void {
-    
+  constructor(private contactPopupService: ContactPopupService){
+  }
+
+  ngOnInit(): void {
+    this.contactPopupService.openButtonShowStatus$.subscribe((value) => {
+      this.ownerOpen = value;
+      console.log(this.ownerOpen + " En openPopup method")
+    });
+  }
+  
+  openPopup(id: number, index: number){
+    const popupContainer = this.popupContainers.get(index)
+    if (popupContainer){
+      this.contactPopupService.openPopup(popupContainer, id)
+      
+    }
   }
 }
