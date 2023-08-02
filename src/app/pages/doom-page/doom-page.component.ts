0ff445fb953442faaaf1e73e65f20359f018ee3c
Danilo Del Collado <daniloddc.92@gmail.com>
2023-08-02 10:23:50 -0300

    changes on doom componnet

--- src/app/pages/doom-page/doom-page.component.ts
+++ src/app/pages/doom-page/doom-page.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {*} from 'src/assets/doom-game'
 
 @Component({
   selector: 'app-doom-page',
   templateUrl: './doom-page.component.html',
   styleUrls: ['./doom-page.component.scss']
 })
-export class DoomPageComponent {
+export class DoomPageComponent implements OnInit{
+  doomFilesReady: boolean = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.doomFilesReady = true
+    }, 3000)
+
 
 }
